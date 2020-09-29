41c90fcf338668e4270b3caa20010916f107c79d
sam253narula <samarthnarula253@gmail.com>
2020-09-29 22:09:51 +0530

    first commit

---
+++ src/main/java/com/springboot/aws/lambda/OrderHandler.java
@@ -0,0 +1,6 @@
+package com.springboot.aws.lambda;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class OrderHandler extends SpringBootRequestHandler<String,Object> {
+}
---
+++ src/main/java/com/springboot/aws/lambda/SpringbootAwsLambdaApplication.java
@@ -0,0 +1,37 @@
+package com.springboot.aws.lambda;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.springboot.aws.lambda.domain.Order;
+import com.springboot.aws.lambda.respository.OrderDao;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class SpringbootAwsLambdaApplication {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @Bean
+    public Supplier<List<Order>> orders() {
+        return () -> orderDao.buildOrders();
+    }
+
+    @Bean
+    public Function<String, List<Order>> findOrderByName() {
+        return (input) -> orderDao.buildOrders().stream().filter(order -> order.getName().equals(input)).collect(Collectors.toList());
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAwsLambdaApplication.class, args);
+    }
+
+}
